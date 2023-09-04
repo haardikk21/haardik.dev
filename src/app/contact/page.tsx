5e84c24caee650a1d1354932c6bf33d5ca0057b2
Haardik <hhaardik@uwaterloo.ca>
2023-09-04 10:15:01 -0400

    add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,33 @@
 export default function Contact() {
-  return null;
+  return (
+    <div className="flex w-full items-center justify-between py-8">
+      <div className="flex max-w-prose flex-col justify-between">
+        <div className="flex w-fit items-end border-b pb-2">
+          <h2 className="text-4xl font-semibold">Contact</h2>
+        </div>
+
+        <ul role="list" className="mt-6 flex flex-col gap-2">
+          <li className="flex items-center gap-2">
+            <span className="font-semibold">Email:</span>
+            <a href="mailto:haardik@learnweb3.io">haardik@learnweb3.io</a>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <span className="font-semibold">Twitter:</span>
+            <a href="https://twitter.com/haardikkk">@haardikkk</a>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <span className="font-semibold">Farcaster:</span>
+            <a href="https://warpcast.com/haardikkk">@haardikkk</a>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <span className="font-semibold">Github:</span>
+            <a href="https://github.com/haardikk21">@haardikk21</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/app/writings/page.tsx
+++ src/app/writings/page.tsx
@@ -1,3 +1,23 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
 export default function Writings() {
-  return null;
+  return (
+    <div className="flex w-full items-center justify-between py-8">
+      <div className="flex max-w-prose flex-col justify-between">
+        <div className="flex w-fit items-end border-b pb-2">
+          <h2 className="text-4xl font-semibold">Writings</h2>
+        </div>
+
+        <Card className="mx-auto mt-6 w-fit">
+          <CardHeader>
+            <CardTitle>Work in Progress</CardTitle>
+          </CardHeader>
+          <CardContent>
+            I&apos;m working on aggregating all my work in one place. Stay
+            tuned!
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
